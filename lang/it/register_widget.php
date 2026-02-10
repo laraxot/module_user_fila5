2bb4cf5aa2ae68b22a02cceaa6b4b60ec7a43474
marco76tv <marco.sottana@gmail.com>
2026-02-10 13:52:37 +0100

    .

---
+++ lang/it/register_widget.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'sections' => [
+        'empty' => [
+            'label' => 'empty',
+            'heading' => 'empty',
+        ],
+    ],
+    'fields' => [
+        'first_name' => [
+            'label' => 'first_name',
+            'placeholder' => 'first_name',
+            'helper_text' => 'first_name',
+            'description' => 'first_name',
+        ],
+        'last_name' => [
+            'label' => 'last_name',
+            'placeholder' => 'last_name',
+            'helper_text' => 'last_name',
+            'description' => 'last_name',
+        ],
+        'email' => [
+            'label' => 'email',
+            'placeholder' => 'email',
+            'helper_text' => 'email',
+            'description' => 'email',
+        ],
+        'password_confirmation' => [
+            'description' => 'password_confirmation',
+            'helper_text' => 'password_confirmation',
+            'placeholder' => 'password_confirmation',
+            'label' => 'password_confirmation',
+        ],
+        'privacy_policy_accepted' => [
+            'description' => 'privacy_policy_accepted',
+            'helper_text' => 'privacy_policy_accepted',
+            'placeholder' => 'privacy_policy_accepted',
+        ],
+        'marketing_consent' => [
+            'description' => 'marketing_consent',
+            'helper_text' => 'marketing_consent',
+            'placeholder' => 'marketing_consent',
+            'label' => 'marketing_consent',
+        ],
+        'password' => [
+            'description' => 'password',
+            'helper_text' => 'password',
+            'placeholder' => 'password',
+        ],
+        'third_party_consent' => [
+            'description' => 'third_party_consent',
+            'helper_text' => 'third_party_consent',
+            'placeholder' => 'third_party_consent',
+            'label' => 'third_party_consent',
+        ],
+        'analytics_consent' => [
+            'description' => 'analytics_consent',
+            'helper_text' => 'analytics_consent',
+            'placeholder' => 'analytics_consent',
+            'label' => 'analytics_consent',
+        ],
+        'profiling_consent' => [
+            'description' => 'profiling_consent',
+            'helper_text' => 'profiling_consent',
+        ],
+    ],
+];
