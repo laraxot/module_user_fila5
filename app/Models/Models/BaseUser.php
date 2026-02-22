5b80a0813dc5166cda4c72e5dff8839de653b77d
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-02-22 20:58:28 +0000

    Check & fix styling

--- app/Models/Models/BaseUser.php
+++ app/Models/Models/BaseUser.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\User\Models\Models;
 
-use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Notifications\Notifiable;
 
 abstract class BaseUser extends Authenticatable
@@ -22,7 +22,7 @@ abstract class BaseUser extends Authenticatable
     {
         /** @var class-string<DatabaseNotification> $notificationClass */
         $notificationClass = config('notifications.notification_model', DatabaseNotification::class);
-        
+
         return $this->morphMany($notificationClass, 'notifiable');
     }
 }
