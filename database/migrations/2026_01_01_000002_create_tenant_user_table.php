207c0bc7d8cc6b5ec3fd1f9dd2820ed603d27f7c
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-03-06 12:54:34 +0000

    Check & fix styling

--- database/migrations/2026_01_01_000002_create_tenant_user_table.php
+++ database/migrations/2026_01_01_000002_create_tenant_user_table.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Xot\Database\Migrations\XotBaseMigration;
 
-use function Safe\file_put_contents;
-
 return new class extends XotBaseMigration {
     /**
      * Run the migrations.
