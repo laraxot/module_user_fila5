04b5ecad97e618f6bdc56ca6e9e4d4aaee8d3d65
marco76tv <marco.sottana@gmail.com>
2026-03-23 18:49:44 +0100

    .

--- app/Models/OauthAccessToken.php
+++ app/Models/OauthAccessToken.php
@@ -10,16 +10,16 @@ use Modules\User\Database\Factories\OauthAccessTokenFactory;
 
 /**
  * Passport Token model for User module.
- *
+ * 
  * Extends Laravel\Passport\Token to provide custom behavior
  * and use the user connection for multi-tenancy.
  *
- * @property string              $id
- * @property string              $user_id
- * @property string              $client_id
- * @property string|null         $name
- * @property string|null         $scopes
- * @property bool                $revoked
+ * @property string $id
+ * @property string $user_id
+ * @property string $client_id
+ * @property string|null $name
+ * @property string|null $scopes
+ * @property bool $revoked
  * @property \Carbon\Carbon|null $expires_at
  * @property \Carbon\Carbon|null $created_at
  * @property \Carbon\Carbon|null $updated_at
