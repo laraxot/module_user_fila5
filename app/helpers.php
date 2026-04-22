30dd48ea46f316970c26d70447320b4e35162c6b
marco76tv <marco.sottana@gmail.com>
2026-04-22 08:32:43 +0200

    .

---
+++ app/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+// Project helper functions placeholder to satisfy composer autoload.
+
+if (! function_exists('module_helper_placeholder')) {
+    function module_helper_placeholder(): void
+    {
+        // intentionally empty
+    }
+}
