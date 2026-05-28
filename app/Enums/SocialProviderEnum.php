1cf1cb6484c4b5426de274bb4e89f51c698adc76
Malebestia <malebestia@yahoo.it>
2026-05-28 18:58:24 +0200

    .

--- app/Enums/SocialProviderEnum.php
+++ app/Enums/SocialProviderEnum.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\User\Enums;
 
-enum SocialProviderEnum: string
+use Filament\Support\Contracts\HasLabel;
+use Modules\Xot\Traits\EnumTrait;
+
+enum SocialProviderEnum: string implements HasLabel
 {
+    use EnumTrait;
     case GOOGLE = 'google';
     case AUTH0 = 'auth0';
 }
