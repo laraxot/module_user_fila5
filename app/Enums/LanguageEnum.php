b2cd53abdf8ac4a440c304d85055857973aa20d6
marco76tv <marco.sottana@gmail.com>
2026-05-29 08:41:13 +0200

    .

--- app/Enums/LanguageEnum.php
+++ app/Enums/LanguageEnum.php
@@ -4,24 +4,19 @@ declare(strict_types=1);
 
 namespace Modules\User\Enums;
 
+use Modules\Xot\Traits\EnumTrait;
 use Filament\Support\Contracts\HasLabel;
 
 enum LanguageEnum: string implements HasLabel
 {
+    use EnumTrait;
+
     case ITALIAN = 'it';
     case ENGLISH = 'en';
     case FRENCH = 'fr';
     case GERMAN = 'de';
     case SPANISH = 'es';
 
-    public function getLabel(): string
-    {
-        return match ($this) {
-            self::ITALIAN => 'Italiano',
-            self::ENGLISH => 'English',
-            self::SPANISH => 'Español',
-            self::FRENCH => 'Français',
-            self::GERMAN => 'Deutsch',
-        };
+    ;
     }
 }
