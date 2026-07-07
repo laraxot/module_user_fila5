9f31997d07b694ef72ae18200fb515fed73942d0
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-07 15:10:45 +0000

    Check & fix styling

--- app/Traits/PasswordValidationRules.php
+++ app/Traits/PasswordValidationRules.php
@@ -12,11 +12,12 @@ trait PasswordValidationRules
     /**
      * Get the validation rules used to validate passwords.
      *
-<<<<<<< HEAD
+     * <<<<<<< HEAD
+     *
      * @return array<int, (Rule|array|string)>
-=======
+     *                                         =======
      * @return array<int, Password|string>
->>>>>>> 9fa499be (.)
+     *                                         >>>>>>> 9fa499be (.)
      */
     protected function passwordRules(): array
     {
