b1fe3dee3d111e556e3b2dfb9e431098f6f6b6b6
marco76tv <marco.sottana@gmail.com>
2026-07-21 14:15:27 +0200

    .

--- database/factories/DeviceProfileFactory.php
+++ database/factories/DeviceProfileFactory.php
@@ -4,37 +4,27 @@ declare(strict_types=1);
 
 namespace Modules\User\Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\User\Models\DeviceProfile;
 
 /**
- * DeviceProfile Factory.
- *
- * Factory for creating DeviceProfile model instances for testing and seeding.
- * Extends DeviceUserFactory since DeviceProfile extends DeviceUser.
+ * @extends Factory<DeviceProfile>
  */
-class DeviceProfileFactory extends DeviceUserFactory
+class DeviceProfileFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
-     *
-     * @var class-string<DeviceProfile>
      */
     protected $model = DeviceProfile::class;
 
     /**
      * Define the model's default state.
-     * Inherits from DeviceUserFactory and adds profile-specific attributes.
-     *
+     */
+    /**
      * @return array<string, mixed>
      */
-    #[\Override]
     public function definition(): array
     {
-        return array_merge(
-            parent::definition(),
-            [
-                // DeviceProfile-specific attributes can be added here if needed
-            ],
-        );
+        return [];
     }
 }
