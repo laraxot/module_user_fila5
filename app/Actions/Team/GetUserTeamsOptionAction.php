a123b38d40636d6d4d1aca43b865d85fffc839ab
marco76tv <marco.sottana@gmail.com>
2026-08-04 19:11:28 +0200

    .

---
+++ app/Actions/Team/GetUserTeamsOptionAction.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Actions\Team;
+
+use Modules\User\Models\TeamUser;
+use Spatie\QueueableAction\QueueableAction;
+
+class GetUserTeamsOptionAction
+{
+    use QueueableAction;
+
+    /**
+     * @return array<int|string, string>
+     */
+    public function execute(): array
+    {
+        $teams = TeamUser::where('user_id', authId())->get();
+
+        /** @var array<int|string, string> $options */
+        $options = ['' => '--- Select ---'];
+
+        foreach ($teams as $teamUser) {
+            $team = $teamUser->team;
+            if ($team === null) {
+                continue;
+            }
+
+            $key = $team->getKey();
+            $name = $team->getAttribute('name');
+            $options[is_string($key) ? $key : (string) $key] = is_string($name) ? $name : (string) $name;
+        }
+
+        return $options;
+    }
+}
---
+++ lang/it/socialite_user.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'provider' => [
+            'label' => 'provider',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'provider_id' => [
+            'label' => 'provider_id',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'name' => [
+            'label' => 'name',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'email' => [
+            'label' => 'email',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'avatar' => [
+            'label' => 'avatar',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'create' => [
+            'label' => 'create',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'attach' => [
+            'label' => 'attach',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'layout' => [
+            'label' => 'layout',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'view' => [
+            'label' => 'view',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'edit' => [
+            'label' => 'edit',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'detach' => [
+            'label' => 'detach',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+        'toggleColumns' => [
+            'label' => 'toggleColumns',
+            'tooltip' => '',
+            'helper_text' => '',
+            'description' => '',
+        ],
+    ],
+    'navigation' => [
+        'sort' => 89,
+        'icon' => 'heroicon-o-user',
+        'group' => 'Authentication',
+        'label' => 'Social Authentications',
+    ],
+    'label' => 'Socialite User',
+    'plural_label' => 'Socialite User (Plurale)',
+    'actions' => [
+        'create' => [
+            'label' => 'Crea Socialite User',
+        ],
+        'edit' => [
+            'label' => 'Modifica Socialite User',
+        ],
+        'delete' => [
+            'label' => 'Elimina Socialite User',
+        ],
+    ],
+];
