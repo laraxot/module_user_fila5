7ebbe8d96576016db67e9ee831d190ec49f545d3
Marco Xot <marco76tv@hotmail.com>
2026-08-06 19:34:11 +0200

    .

--- app/Filament/Resources/OauthAccessTokenResource.php
+++ app/Filament/Resources/OauthAccessTokenResource.php
@@ -65,7 +65,7 @@ class OauthAccessTokenResource extends XotBaseResource
                             return null;
                         }
                         $user = $record->user;
-                        if (null !== $user && method_exists($user, 'exists') && $user->exists) {
+                        if ($user !== null && method_exists($user, 'exists') && $user->exists) {
                             return UserResource::getUrl('view', ['record' => $user]);
                         }
 
@@ -84,7 +84,7 @@ class OauthAccessTokenResource extends XotBaseResource
                 TextColumn::make('scopes')
                     ->limit(30)
                     ->tooltip(function (mixed $state): ?string {
-                        if (null === $state) {
+                        if ($state === null) {
                             return null;
                         }
                         if (is_array($state)) {
