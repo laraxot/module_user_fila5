63c0770df34e88a5ba277c2f4edbb4701c07a481
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-14 15:35:24 +0000

    Check & fix styling

--- app/Models/BaseUser.php
+++ app/Models/BaseUser.php
@@ -11,7 +11,6 @@ use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
