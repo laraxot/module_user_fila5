f0f571d0865188fd88126c3b40cb4c33f9001c64
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-09-15 05:52:39 +0000

    Check & fix styling

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -14,16 +14,16 @@ use PHPUnit\Framework\Assert;
 uses(TestCase::class);
 
 /**
-<<<<<<< .merge_file_XTEc1X
- * @param array<string, mixed> $attributes
-=======
- * <<<<<<< HEAD.
+ * <<<<<<< .merge_file_XTEc1X.
  *
+ * @param array<string, mixed> $attributes
+ *                                         =======
+ *                                         <<<<<<< HEAD
  * @param array<string, mixed> $attributes
  *                                         =======
  * @param array<string, mixed> $attributes
  *                                         >>>>>>> laraxot/dev
->>>>>>> .merge_file_EJldfA
+ *                                         >>>>>>> .merge_file_EJldfA
  */
 function modelsUserCreate(array $attributes = []): User
 {
